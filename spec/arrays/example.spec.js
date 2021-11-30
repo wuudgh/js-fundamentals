7fef1a85e53b110ed62636973b37de16f7a3cda6
Mike <mike.m.herron@googlemail.com>
2021-11-30 16:32:16 +0000

    Added Arrrays examples

---
+++ spec/arrays/example.spec.js
@@ -0,0 +1,12 @@
+const example = require('../../src/arrays/example.js')
+
+describe("Arrays example", () => {
+  it("Array Example'", () => {
+    expect(example.a).toEqual("This is the first item")
+    expect(example.b).toEqual("This is the second item")
+    expect(example.c).toEqual(2)
+    expect(example.d).toEqual([1,2,3])
+    expect(example.e).toEqual(["a","b","c","d"])
+    expect(example.f).toEqual([100, 101, 102])
+  })
+})
---
+++ src/arrays/example.js
@@ -0,0 +1,31 @@
+const items = ['This is the first item', 'This is the second item']
+const e = ['a', 'b', 'c']
+const f = [101, 102]
+
+// TODO: Make a the value of the first item
+const a = items[0]
+
+// TODO: Make b the value of the second item
+const b = items[1]
+
+// TODO: Make c the value of the total number of items in the list
+const c = items.length
+
+// TODO: Make d an array with the numbers 1, 2, 3
+const d = [1, 2, 3]
+
+// TODO: Call a method on e to add "d" to the end of the array
+e.push('d')
+
+// TODO: Call a method on f to add 100 to the start of the array
+f.unshift(100)
+
+// Do not edit this exported object
+module.exports = {
+  a: a,
+  b: b,
+  c: c,
+  d: d,
+  e: e,
+  f: f
+}
