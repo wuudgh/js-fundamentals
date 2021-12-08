c3745424ac69a74208439b8510d18d0207f841d2
mikemherron <mike.m.herron@googlemail.com>
2021-12-08 13:29:45 +0000

    Update assignment.js

--- src/variables/assignment.js
+++ src/variables/assignment.js
@@ -1,10 +1,9 @@
 // do not edit this
 let anotherNumber2 = 10
 
-// TODO: Change the values of the variables below so that the tests pass
-// You will need to add a new line to update the value of anotherNumber3
-const anotherNumber = NaN // edit this value
-
+// TODO: Change the value of the anotherNumber below so that the tests pass
+// You will also need to reassign the value of anotherNumber2
+const anotherNumber = 8 // edit this value
 
 // do not edit this
 const count = 2
