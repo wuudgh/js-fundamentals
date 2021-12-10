e2c580f39bb7016a00fe4960cba20798fb85680b
Mike <mike.m.herron@googlemail.com>
2021-12-10 14:23:54 +0000

    Add another assignment to prevent linter from auto-chaning let to const

--- src/loops/for-loop-and-arrays.js
+++ src/loops/for-loop-and-arrays.js
@@ -1,13 +1,15 @@
 const nums = [1, 3, 12, 5, 1, 6, 4, 1, 10]
 const letters = ['H', 'e', 'l', 'l', 'o']
+let sum = 0
+let word = ''
 
 // TODO: Add code below this line to make the tests pass
 
-const sum = 0 // Use a for loop to set the sum variable to the sum of all the values in nums
+sum = 0 // Use a for loop to set the sum variable to the sum of all the values in nums
 
 const doubledNums = [] // Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 
-const word = '' // Use a for loop to set word equal to all the letters in the letters array
+word = '' // Use a for loop to set word equal to all the letters in the letters array
 
 const everySecondNum = [] // Use a for loop to populate everySecondNum with every second number from the nums array
 
