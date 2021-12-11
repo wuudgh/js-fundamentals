20d2a19b1acffd901b3afd7d772697272a27a62d
Mike <mike.m.herron@googlemail.com>
2021-12-11 17:54:29 +0000

    Fix typo

--- src/data-types/strings.js
+++ src/data-types/strings.js
@@ -1,17 +1,21 @@
 // do not edit these lines
-const alphabet = 'ABCDEFGHIJLKLMNOPQRSTUVWYZ'
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const firstName = 'Jane'
 const secondName = 'Smith'
 
 // TODO: Update the code using Javascript string operations and the variables above so that the tests pass.
 
-const fullName = null // Concatenate firstName and secondName
+// Concatenate firstName and secondName
+const fullName = null
 
-const tenthCharacterOfAlphabet = null // Get the 10th character of the alphabet variable
+// Get the 10th character of the alphabet variable
+const tenthCharacterOfAlphabet = null
 
-const lowerCaseAlphabet = null // Call a method on the alphabet variable to transform it to lower case
+// Call a method on the alphabet variable to transform it to lower case
+const lowerCaseAlphabet = null
 
-const numberOfLettersInAlphabet = null // USe a property on the alphabet variable to get it's length
+// Use a property on the alphabet variable to get it's length
+const numberOfLettersInAlphabet = null 
 
 // do not edit the exported object.
 module.exports = {
