268998117c52ef57caa34b722a05228cfb2ec9a3
Mike <mike.m.herron@googlemail.com>
2021-12-11 17:50:55 +0000

    Add additional multiple condition exercise

--- spec/conditional-flow/multiple-conditions.spec.js
+++ spec/conditional-flow/multiple-conditions.spec.js
@@ -1,4 +1,4 @@
-const {a, b} = require('../../src/conditional-flow/multiple-conditions')
+const {a, b, c} = require('../../src/conditional-flow/multiple-conditions')
 
 describe("isInRange", () => {
   it("4 is between 1 and 6", () => {
@@ -27,4 +27,40 @@ describe("isHelloOrGoodbye", () => {
   it("'Goodbye' returns true", () => {
     expect(b('Goodbye')).toEqual(true)
   })
+})
+  
+describe("getAgeDescription", () => {
+  it("0 is a Baby", () => {
+    expect(c(0)).toEqual("Baby")
+  })
+  it("1-4 is a Toddler", () => {
+    expect(c(1)).toEqual("Toddler")
+    expect(c(2)).toEqual("Toddler")
+    expect(c(3)).toEqual("Toddler")
+    expect(c(4)).toEqual("Toddler")
+  })
+  it("5-12 is a Child", () => {
+    expect(c(5)).toEqual("Child")
+    expect(c(6)).toEqual("Child")
+    expect(c(7)).toEqual("Child")
+    expect(c(9)).toEqual("Child")
+    expect(c(10)).toEqual("Child")
+    expect(c(11)).toEqual("Child")
+    expect(c(12)).toEqual("Child")
+  })
+  it("13-19 is a Teenager", () => {
+    expect(c(13)).toEqual("Teenager")
+    expect(c(14)).toEqual("Teenager")
+    expect(c(15)).toEqual("Teenager")
+    expect(c(16)).toEqual("Teenager")
+    expect(c(17)).toEqual("Teenager")
+    expect(c(18)).toEqual("Teenager")
+    expect(c(19)).toEqual("Teenager")
+  })
+  it("20+ is an Adult", () => {
+    expect(c(20)).toEqual("Adult")
+    expect(c(25)).toEqual("Adult")
+    expect(c(40)).toEqual("Adult")
+  })
+
 })
--- src/conditional-flow/multiple-conditions.js
+++ src/conditional-flow/multiple-conditions.js
@@ -16,7 +16,26 @@ function isHelloOrGoodbye (val1) {
 
 }
 
+// This function should return a string that describes the provided age value. The 
+// table below shows for each range of age values what string should be returned.
+//
+// For example, if an age of 3 is provided to the function, "Toddler" should be
+// returned.
+//
+// Input   Output
+// 0      | Baby
+// 1-4    | Toddler
+// 5-12   | Child
+// 13-19  | Teenager
+// 20+    | Adult
+function getAgeDescription (age) {
+  
+  // TODO: write code in this function body to pass the tests
+}
+
+
 module.exports = {
   a: isInRange,
-  b: isHelloOrGoodbye
+  b: isHelloOrGoodbye,
+  c: getAgeDescription
 }
