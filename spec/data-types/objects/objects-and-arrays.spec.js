73744435c2fb29b35d1ba3ad7ca0bde53d5fc41b
Mike <mike.m.herron@googlemail.com>
2021-12-12 17:40:17 +0000

    Clearer error messages in objects and arrays

--- spec/data-types/objects/objects-and-arrays.spec.js
+++ spec/data-types/objects/objects-and-arrays.spec.js
@@ -1,11 +1,11 @@
 const answers = require('../../../src/data-types/objects/objects-and-arrays.js')
 
 describe('Objects and Arrays:', () => {
-  it('Number of voucher codes should be 2', () => {
+  it('numberOfVoucherCodes should be 2', () => {
     expect(answers.numberOfVoucherCodes).toEqual(2)
   })
 
-  it('First voucher code should be AA-AA-A', () => {
+  it('firstVoucherCode should be AA-AA-A', () => {
     expect(answers.firstVoucherCode).toEqual("AA-AA-A")
   })
 
