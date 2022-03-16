aed0e7230f3afdf012a604e0435f2861f2848aac
DWAUUD BELKO <belkodawuud@gmail.com>
2022-03-16 10:56:40 +0100

    new changes

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -9,7 +9,25 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+//To write a a function i first need the function key word
+//then the name i give to the function.
+//Two pair of brackets where i write the parameters or the input.
+//Another pair of curlly brackets where i write the block of code telling the function what i want it to do.
+//The last part is the return,  where i write the  output of the function.
+//I will also need a for loop which begins from lower to upper
+//So i will initialise is from lower.
+//And make it run to less than or equals to upper.
+//I keep increasing any time the loop runs.
+//I also need to create an empty array where i push all the output on.
+//I also need a prototype method .push().
 
+function lowerUpper(lower, upper) {
+  let nums = [];
+  for (let i = lower; i <= upper; i++) {
+    nums.push(i);
+  }
+  return nums;
+}
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,9 +39,25 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+//To write a function i need the function key word.
+//I also need to give the function a name.
+//The function will also need a pair of bracket () where i write the parameters or the input.
+//I will also need a pair of curly brackets where i write the block of code tell the function what i want it to do.
+//I last part is the return where i return the out put of the function.
+//To turn a string of lowercase to uppercase i need a prototype method call toUpperCase()
+// I will also need a for loop.
+// I need a method of joining two strings together call string concatenation.
+
+function twoArgs(word, num) {
+  for (let i = 0; i < num; i++) {
+    word += "!";
+  }
+  const newWord = word.toUpperCase();
+  return newWord;
+}
 
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: twoArguments, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: newName, // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: lowerUpper, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: twoArgs, // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 };
--- src/functions/creating-functions.js
+++ src/functions/creating-functions.js
@@ -26,9 +26,35 @@ function addOne(number) {
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+//To define a function i need the function key word.
+//Followed by the function name
+//After the name i need a pair of bracket where i pass the input of the function.
+//Plus i also need a pair of curly bracket where we tell the function what i want it to do.
+//The finall part is the return where we find our output.
+//To make a capitalized i need a proto type method call toUpperCase()
+//But before i do that i have to create a variable where i concatinate the functions parameter and the trings,
+//which i want to join together with the parameter, i do that by applying the + sign between them.
+//Correction: i thought i can use the prototype method toUpperCase within the variable where i concatenated
+//the parameter and the string to change them to uppercase, but that didn't work because the protype toUpperCase
+//does not change the value of what ever you give it, But it rather gives you a new value in upperCase.
+//Correction: I am suppose to change only the first leter of the word to an upperCase not the whole word.
+//To do that i have to the target the first index of the word which is basically the the first letter.
+//e.g. letter[0] since java script starts counting from 0.
+//That alone wasn't enough to wet what i wanted so researched and found prototype method call slice()
+//I didn't really understand it but it worked for me.
+//
 
+function nameSmiley(name) {
+  const newName = name[0].toUpperCase() + name.slice(1);
+  const hi = "Hi";
+  const concat = hi + ", " + newName + " :)";
+  return concat;
+}
+nameSmiley("edmond");
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
   a: addOne, // change undefined to be the name of the function you defined to increment a number (the first TODO)
-  b: undefined, // change undefined to be the name of the function you defined to say hi (the second TODO)
+  b: nameSmiley, // change undefined to be the name of the function you defined to say hi (the second TODO)
 };
+
+//npx jasmine spec/functions/creating-functions.spec.js
